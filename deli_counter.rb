434974aa51b2e2bacd0a73c934d893224ea3d548
Tom Holmes <tholmes59@gmail.com>
2019-01-16 23:13:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,12 @@ def line(array)
   puts "The line is currently: #{line_array.join(" ")}"
   end
 end
+
+def take_a_number(array, name)
+end
+
+
+def now_serving(array)
+  if array.length == 0 
+    puts 
+end
