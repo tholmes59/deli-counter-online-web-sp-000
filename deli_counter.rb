aed9cc83efd3ebdf55f823c6183a376bb5a22234
Tom Holmes <tholmes59@gmail.com>
2019-01-16 22:06:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+    elsif array.length > 0 
+    puts "The line is currently: "
+  end
+end
