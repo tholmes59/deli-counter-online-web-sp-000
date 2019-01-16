68af1e5bc2bb85b3a6006f58572c2a5fce630bd2
Tom Holmes <tholmes59@gmail.com>
2019-01-16 23:27:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(array, name)
   array << "#{name}"
-    puts "Welcome, #{name}. You are number #{index} in line."
+    puts "Welcome, #{name}. You are number #{array.length} in line."
   end
 end
 
